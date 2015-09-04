6482f517ee28684c8538d1e67364e0f3ad74cfe2
Jaroslav Kuchař <jaroslav.kuchar@gmail.com>
2015-09-04 18:16:23 +0200

    Update default.rb

--- cookbooks/mimdw/recipes/default.rb
+++ cookbooks/mimdw/recipes/default.rb
@@ -11,9 +11,56 @@ end
 
 # Desktop Environment
 package "xfce4"
+package "virtualbox-guest-dkms"
+package "virtualbox-guest-utils"
+package "virtualbox-guest-x11"
+package "slim"
+
+# vagrant reload
 
 # Chrome
 package "chromium-browser"
+
+# Eclipse
+remote_file '/tmp/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz' do
+  source 'http://mirrors.nic.cz/eclipse/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz'
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0755'
+  action :create
+  not_if { File.exist?('/home/vagrant/Desktop/eclipse/') }
+end
+
+execute 'extract_eclipse' do
+  command 'mkdir /home/vagrant/Desktop && tar xzvf eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz -C /home/vagrant/Desktop/'
+  cwd '/tmp/'
+  user 'vagrant'
+  group 'vagrant'
+  not_if { File.exists?("/home/vagrant/Desktop/eclipse/") }
+end
+
+# https://github.com/snowch/carbon-products-development-environment/blob/2aab91bbde6f2564f1b72f4908e881083519f560/scripts/desktop_setup.sh
+bash "eclipse script" do
+  user "vagrant"
+  code <<-EOH  		
+	echo '#!/bin/bash' >> /home/vagrant/Desktop/eclipse-start.sh
+	echo 'export SWT_GTK3=0 && /home/vagrant/Desktop/eclipse/eclipse' >> /home/vagrant/Desktop/eclipse-start.sh
+	chmod +x /home/vagrant/Desktop/eclipse-start.sh
+  EOH
+  not_if { File.exists?("/home/vagrant/Desktop/eclipse/eclipse-start.sh") }
+end
+
+bash "server script" do
+  user "vagrant"
+  code <<-EOH  		
+	echo '#!/bin/bash' >> /home/vagrant/Desktop/server-start.sh
+	echo 'x-terminal-emulator -e /vagrant/oracle_mw/user_projects/domains/base_domain/startWebLogic.sh && wait $!' >> /home/vagrant/Desktop/server-start.sh
+	chmod +x /home/vagrant/Desktop/server-start.sh
+  EOH
+  not_if { File.exists?("/home/vagrant/Desktop/eclipse/server-start.sh") }
+end
+
+# http://mirrors.nic.cz/eclipse/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz
 
 # # Generate scripts
 # execute "generate scripts" do
@@ -24,16 +71,16 @@ package "chromium-browser"
 
 # # WLS
 # execute "wls" do
-# 	command "/vagrant/oracle_scripts/wls-silent.sh > /vagrant/oracle_scripts/wls-silent.lof"
+# 	command "/vagrant/oracle_scripts/wls-silent.sh > /vagrant/oracle_scripts/wls-silent.log"
 # 	user "root"
-# 	not_if { ::File.exists?("/vagrant/oracle_mw/wlserver")}
+# 	not_if { ::File.exists?("/vagrant/oracle_mw/wlserver/common/bin/config.sh")}
 # end
 
 # # OSB
 # execute "osb" do
 # 	command "/vagrant/oracle_scripts/osb-silent.sh > /vagrant/oracle_scripts/osb-silent.log"
 # 	user "vagrant"
-# 	not_if { ::File.exists?("/vagrant/oracle_mw/Oracle_OSB1")}
+# 	not_if { ::File.exists?("/vagrant/oracle_mw/Oracle_OSB1/install/setupinfo.txt")}
 # end
 
 # # Domain
